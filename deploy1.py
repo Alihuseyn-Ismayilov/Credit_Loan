94bc35a52988908a954e1539464fb92b96a4ae8e
Alihuseyn-Ismayilov <70060464+Alihuseyn-Ismayilov@users.noreply.github.com>
2023-06-04 13:52:56 +0400

    Update deploy1.py

--- deploy1.py
+++ deploy1.py
@@ -37,15 +37,15 @@ model = joblib.load(model_path)
 with st.form('my_form'):
     col1, col2 = st.columns(2)
     with col1:
-        v1 = st.number_input(label='revolvingutilizationofunsecuredlines',value=0.031530,format='%4f')
-        v2 = st.number_input(label='age',value=59,format='%d')
-        v3 = st.number_input(label='numberoftime30_59dayspastduenotworse',value=0,format='%d')
-        v4 = st.number_input(label='debtratio',value=0.003737,format='%6f')
+        v1 = st.number_input(label='Revolving Utilization of Unsecured Lines',value=0.031530,format='%4f')
+        v2 = st.number_input(label='Age',value=59,format='%d')
+        v3 = st.number_input(label='Number of Time 30-59 Days Past Due Not worse',value=0,format='%d')
+        v4 = st.number_input(label='Debt Ratio',value=0.003737,format='%6f')
     with col2:
-        v5 = st.number_input(label='monthlyincome',value=9900.0,format='%1f')
-        v6 = st.number_input(label='numberofopencreditlinesandloans',value=3,format='%d')
-        v7 = st.number_input(label='numberrealestateloansorlines',value=0,format='%d')
-        v8 = st.number_input(label='numberofdependents',value=0,format='%d')
+        v5 = st.number_input(label='Monthly Income',value=9900.0,format='%1f')
+        v6 = st.number_input(label='Number Of Open Credit Linesand Loans',value=3,format='%d')
+        v7 = st.number_input(label='Number Real Estate Loans or Lines',value=0,format='%d')
+        v8 = st.number_input(label='Number Of Dependents',value=0,format='%d')
     v9 = st.radio("Have late payments?",("Yes","No"))
     v = 0
     if(v9 == "Yes"):
@@ -53,7 +53,7 @@ with st.form('my_form'):
     submit = st.form_submit_button('submit')
 predictors = [[v1,v2,v3,v4,v5,v6,v7,v8,v]]
 
-#st.write(predictors)
+st.write('Will person experience 90 days past due delinquency or worse? Yes/NO')
 if(model.predict(predictors)[0] == 0):
     st.write('No')
 else:
